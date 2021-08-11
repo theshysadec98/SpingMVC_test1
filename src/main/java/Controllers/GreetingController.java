76d533669939d10ef9f11c59c69e216f2a7a404e
theshysadec98 <lyminhhoang01041998@gmail.com>
2021-08-11 16:40:43 +0700

    up

---
+++ src/main/java/Controllers/GreetingController.java
@@ -0,0 +1,15 @@
+package Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("show")
+    public String show(@RequestParam String name, Model model){
+        model.addAttribute("name", name);
+        return "/WEB-INF/views/test.jsp";
+    }
+}
